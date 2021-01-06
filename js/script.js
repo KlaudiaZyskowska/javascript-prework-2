b59d192c4d42e8cd1ee78d23b787de25c55aaeff
Klaudia Zyskowska <klaudiazzyskowska@gmail.com>
2021-01-06 19:41:29 +0100

    Correct the code in script.js file

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 {
-    const playGame = function(playerInput) {
+    const playGame = function (playerInput) {
 
         clearMessages();
 
-        const getMoveName = function(argMoveId) {
+        const getMoveName = function (argMoveId) {
             if (argMoveId == 1) {
                 return 'kamień';
             } else if (argMoveId == 2) {
@@ -19,14 +19,6 @@
 
         const argComputerMove = getMoveName(randomNumber);
 
-        /* if (randomNumber == '1') {
-            computerMove = 'kamień';
-        } else if (randomNumber == '2') {
-            computerMove = 'papier';
-        } else if (randomNumber == '3') {
-            computerMove = 'nożyce';
-        } */
-
         printMessage('Mój ruch to: ' + argComputerMove);
 
         /* let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.'); */
@@ -35,17 +27,9 @@
 
         const argPlayerMove = getMoveName(playerInput);
 
-        /* if (playerInput == '1') {
-            playerMove = 'kamień';
-        } else if (playerInput == '2') {
-            playerMove = 'papier';
-        } else if (playerInput == '3') {
-            playerMove = 'nożyce';
-        } */
-
         printMessage('Twój ruch to: ' + argPlayerMove);
 
-        const displayResult = function(argComputerMove, argPlayerMove) {
+        const displayResult = function (argComputerMove, argPlayerMove) {
             if (argComputerMove == 'kamień' && argPlayerMove == 'papier') {
                 printMessage('Ty wygrywasz!');
             } else if (argComputerMove == 'papier' && argPlayerMove == 'kamień') {
@@ -65,7 +49,7 @@
             }
         }
 
-        let change1 = displayResult(argComputerMove, argPlayerMove);
+        displayResult(argComputerMove, argPlayerMove);
 
         console.log('moves:', argComputerMove, argPlayerMove);
 
